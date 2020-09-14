1dc3b00403a552b0ce27ded0b7ef4eeed4ba9a9a
Tiago Paiva <tgspaiva@gmail.com>
2020-09-14 17:49:37 +0100

    adding rename

---
+++ rename.py
@@ -0,0 +1,17 @@
+# TP 
+# writes list of continuous expnos into other starting at "start" expno
+
+curdat = CURDATA()
+n = INPUT_DIALOG("n experiment", "Insert the n of experiments")
+n = int(n[0])
+start = INPUT_DIALOG("start rw", "Insert starting point") 
+st  = int(start[0])
+
+WR([curdat[0], str(st), curdat[2], curdat[3]],"n")
+exp = 1
+while exp < n:
+	RE_IEXPNO(show = "n")
+	WR([curdat[0], str(st+1), curdat[2], curdat[3]],"n")
+	exp += 1 
+	st += 1	
+RE([curdat[0], str(st), curdat[2], curdat[3]])
