f57f48a2a7aa20b45c61207f006570efb986158d
Tiago Paiva <tgspaiva@gmail.com>
2020-10-23 10:55:33 +0100

    Update pyzip.py

--- pyzip.py
+++ pyzip.py
@@ -3,12 +3,22 @@
 
 import shutil
 import os 
+import sys.argv as argv
 from os.path import expanduser
+
 
 Desktoppath = expanduser('~/Desktop/')
 curdat = CURDATA()
 
-os.chdir(curdat[3]+"/"+curdat[0])
-out = Desktoppath + curdat[0]
-shutil.make_archive(out, 'zip', os.getcwd())
+if len(argv) == 1:
+
+	os.chdir(curdat[3]+"/"+curdat[0])
+	out = Desktoppath + curdat[0]
+	shutil.make_archive(out, 'zip', os.getcwd())
+
+else:
+	
+	os.chdir(curdat[3]+"/"+curdat[0])
+	out = argv[1] + curdat[0]
+	shutil.make_archive(out, 'zip', os.getcwd())
 
